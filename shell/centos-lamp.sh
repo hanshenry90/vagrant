54c7d2e7913f7e0331e3177262927e3c61fdcafa
Hans Jiang <hanshenry90@gmail.com>
2019-12-19 16:38:14 -0800

    updated centos-lamp.sh

--- shell/centos-lamp.sh
+++ shell/centos-lamp.sh
@@ -1,28 +1,40 @@
 #!/bin/bash
+# update CentOS with any patches
+yum update -y --exclude=kernel
 
-#install nano git
+# install nano git
 yum install -y nano git unzip screen
 
-#install Apache web service and start Apache server
+# install Apache web service and start Apache server
 yum install -y httpd httpd-devel httpd-tools
-chkconfig  --add httpd
-chkconfig httpd start
+chkconfig --add httpd
+chkconfig httpd on 
+# stop Apache server for further config
+service httpd stop
 
-#delete html and create symbolic link for my own html
-cd /var/www/html
-sudo rm -rf html
-sudo ln -s /vagrant /var/www/html
+# delete html and create symbolic link for my own html
+rm -rf /var/www/html
+ln -s /vagrant /var/www/html
+# start Apache server
+service httpd start
 
-#install PHP
+# install PHP
 yum install -y php php-cli php-common php-devel php-mysql
 
-#install MySql database service
+# install MySql database service
 yum install -y mysql mysql-server mysql-devel
+# load mysql on system start up
 chkconfig --add mysqld
-chkconfig mysqld start
+chkconfig mysqld on
+# start mysql server
+service mysqld start
 
 #create mysql database table
-mysql -u root -e "CREATE DATABASE IF NOT EXIST lamp_html_service_test"
+mysql -u root -e "SHOW DATABASES";
+# mysql -u root -e "CREATE DATABASE IF NOT EXIST lamp_html_service_test"
+
+service httpd start
+
 
 
 
